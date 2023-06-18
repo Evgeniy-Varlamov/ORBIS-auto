944083483947a160bca779c3697b9c8ccc4e4525
Evgeniy-Varlamov <51528852+Evgeniy-Varlamov@users.noreply.github.com>
2023-06-19 00:32:05 +0400

    first

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(".slider").slick({
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+$("#form1-input").mask("+7 (999) 999-99-99");
+$("#popup-input").mask("+7 (999) 999-99-99");
+
+
+
